49d52a98c3ce996475e01cc3d2c6cb93cd984fb5
Marina DARDE <marinasatsyk@gmail.com>
2022-01-22 20:38:37 +0100

    mobile version

--- app.js
+++ app.js
@@ -280,7 +280,8 @@ class Application {
         let recipesDoc = document.querySelector(".wrap_recipes");
         recipesDoc.innerHTML = "";
         if (this.MyRecipes.length < 1) {
-            recipesDoc.innerHTML = "<h3>Bien tenté, même joueur, joue encore !</h3>";
+            recipesDoc.innerHTML = `<h3 class="no_recipes">Aucune recette ne correspond à votre critère… vous pouvez
+            chercher « tarte aux pommes », « poisson », etc.</h3>`;
             return;
         }
         this.MyRecipes.map(r => {
